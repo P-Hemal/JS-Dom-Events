319be5cac1a1bd78c6c62f6d94bead6a4e22c3aa
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-20 12:28:58 +0600

    option 2

--- Js/events.js
+++ Js/events.js
@@ -1,5 +1,19 @@
 console.log('This is separate Js file')
 // Option 2 : add onclick function.
-function makeRed(){
+function makeRed() {
     document.body.style.backgroundColor = 'red';
+}
+
+// Option 3 : add onclick function.
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+// Option 4 : add onclick function.
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';
 }
