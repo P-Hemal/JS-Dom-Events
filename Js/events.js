74f113f12084ee62456f1e6cff056ab32a743123
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-20 13:00:11 +0600

    option 5

--- Js/events.js
+++ Js/events.js
@@ -1,4 +1,9 @@
 console.log('This is separate Js file')
+// Option 1 : add onclick function.
+
+//<button onclick="console.log('I`m busy now')">Another Button</button>
+//<button onclick="document.body.style.backgroundColor='yellow'">Warning</button>
+
 // Option 2 : add onclick function.
 function makeRed() {
     document.body.style.backgroundColor = 'red';
@@ -14,6 +19,14 @@ function makeBlue() {
 
 // Option 4 : add onclick function.
 const purpleButton = document.getElementById('make-purple');
-purpleButton.onclick = function makePurple(){
+purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
+}
+
+// Option 5 : add Event Listener function.
+
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
 }
